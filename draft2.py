2a61c50b3b851ec3ef056f5a23fed00a5e5a95d9
VoMaiAnh <93236855+VoMaiAnh@users.noreply.github.com>
2024-02-24 01:08:11 +0100

    Add files via upload

---
+++ draft2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 00:44:14 2024
+
+@author: admin
+"""
+import streamlit as st
+
+def main():
+    st.title("Guess the Number")
+
+    modo = st.sidebar.radio("Selecciona el modo:", ["Partida modo solitario", "Partida 2 jugadores", "Estadística", "Salir"])
+    nivel = st.sidebar.radio("Elige tu nivel:", ["Fácil (20 intentos)", "Medio (12 intentos) ", "Difícil (5 intentos)"])
+
+if __name__ == "__main__":
+    main()
